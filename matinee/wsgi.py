d3c3b2958827cf93346444751fe7773b3cfe0d78
daverin <dep9911@gmail.com>
2023-02-09 20:49:02 +0300

    Edited settings files

--- matinee/wsgi.py
+++ matinee/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+from configurations.wsgi import get_wsgi_application
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "matinee.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Prod")
+
 
 application = get_wsgi_application()
